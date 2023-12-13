6d2255e732ac3f6d27a9431c7e4ea34aa2f6d68a
asmaeSakouri <asmaesakouri5@gmail.com>
2023-12-14 00:02:03 +0100

    added authentication

--- src/main/java/com/example/ebankingbackend/web/BankAccountRestController.java
+++ src/main/java/com/example/ebankingbackend/web/BankAccountRestController.java
@@ -6,6 +6,7 @@ import com.example.ebankingbackend.exceptions.BalanceNotSufficient;
 import com.example.ebankingbackend.exceptions.BankAccountNotFound;
 import com.example.ebankingbackend.service.BankService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.transaction.Transactional;
@@ -18,36 +19,43 @@ import java.util.List;
 @CrossOrigin("*")
 public class BankAccountRestController {
     private BankService bankService;
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     @GetMapping("/{accountId}")
     public BankAccountDTO getBankAccount(@PathVariable String accountId) throws BankAccountNotFound {
     return bankService.getBankAccount(accountId);
     }
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     @GetMapping("")
     public List<BankAccountDTO> bankAccountDTOList(){
         return bankService.bankAccountList();
     }
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     @GetMapping("/{accountId}/operations")
     public List<AccountOperationDTO> getHistory(@PathVariable String accountId){
         return bankService.accountHistory(accountId);
     }
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     @GetMapping("/{accountId}/pageOperations")
     public AccountHistoryDTO getAccountHistory(@PathVariable String accountId,
                                                      @RequestParam(defaultValue = "0") int page,
                                                      @RequestParam(defaultValue = "5") int size) throws BankAccountNotFound {
         return bankService.getAccountHistory(accountId,page,size);
     }
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @PostMapping("/debit")
     public DebitDTO debiter(@RequestBody DebitDTO debitDTO) throws BalanceNotSufficient, BankAccountNotFound {
         this.bankService.debit(debitDTO.getAccountId(),
                 debitDTO.getAmount(),debitDTO.getDescription());
         return debitDTO;
     }
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @PostMapping("/credit")
     public CreditDTO crediter(@RequestBody CreditDTO creditDTO) throws BalanceNotSufficient, BankAccountNotFound {
         this.bankService.credit(creditDTO.getAccountId(),
                 creditDTO.getAmount(),creditDTO.getDescription());
         return creditDTO;
     }
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @PostMapping("/transfer")
     public void transfer(@RequestBody TransferDTO transferDTO) throws BalanceNotSufficient, BankAccountNotFound {
         this.bankService.transfert(transferDTO.getAccountSource(),
